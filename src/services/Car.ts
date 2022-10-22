d5e7cd8f7db418fe54030f3af86dce8423a364e4
Andre Rousselet <andre098@hotmail.com>
2022-10-22 11:59:30 -0300

    feat(service): add CarService implementing IService with ICar type
    create method with zod validations

---
+++ src/services/Car.ts
@@ -0,0 +1,23 @@
+import { carZodSchema, ICar } from '../interfaces/ICar';
+import { IModel } from '../interfaces/IModel';
+import { IService } from '../interfaces/IService';
+import { vehicleZodSchema } from '../interfaces/IVehicle';
+
+class CarService implements IService<ICar> {
+  private _carModel: IModel<ICar>;
+
+  constructor(model: IModel<ICar>) {
+    this._carModel = model;
+  }
+
+  public async create(obj: unknown): Promise<ICar> {
+    const parsedVehicle = vehicleZodSchema.safeParse(obj);
+    const parsedCar = carZodSchema.safeParse(obj);
+    if (!parsedVehicle.success) throw parsedVehicle.error;
+    if (!parsedCar.success) throw parsedCar.error;
+    const parsed = { ...parsedVehicle.data, ...parsedCar.data };
+    return this._carModel.create(parsed);
+  }
+}
+
+export default CarService;

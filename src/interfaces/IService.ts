cb06aa2385c7d617b1d007c1cfba164a6bb41d7e
Andre Rousselet <andre098@hotmail.com>
2022-10-23 19:27:56 -0300

    feat(interface): add update method in IService

--- src/interfaces/IService.ts
+++ src/interfaces/IService.ts
@@ -2,4 +2,5 @@ export interface IService<T> {
   create(obj: unknown): Promise<T>;
   read(): Promise<T[]>;
   readOne(_id: string): Promise<T | null>;
+  update(_id: string, obj: unknown): Promise<T | null>;
 }

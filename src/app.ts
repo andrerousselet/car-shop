60f116d2ed21435024a41014db36147ee74d7fdc
Andre Rousselet <andre098@hotmail.com>
2022-10-24 10:56:45 -0300

    feat(routes): add post, get, put and delete motorcycle routes

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import 'express-async-errors';
 import errorHandler from './middlewares/errorHandler';
 import carRouter from './routes/car';
+import motorcycleRouter from './routes/motorcycle';
 
 const app = express();
 app.use(express.json());
 
-app.use(carRouter);
+app.use('/cars', carRouter);
+app.use('/motorcycles', motorcycleRouter);
 
 app.use(errorHandler);
 

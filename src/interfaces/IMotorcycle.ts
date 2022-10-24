65f54988d3382643e7a886435807ff0bf0ec9d53
Andre Rousselet <andre098@hotmail.com>
2022-10-23 21:43:27 -0300

    feat(interface): add IMotorcycle interface
    extending from IVehicle

---
+++ src/interfaces/IMotorcycle.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+import { vehicleZodSchema } from './IVehicle';
+
+// https://github.com/colinhacks/zod#zod-enums
+const motorcycleZodSchema = vehicleZodSchema.extend({
+  category: z.enum(['Street', 'Custom', 'Trail']),
+  engineCapacity: z.number().min(0).max(2500),
+});
+
+export type IMotorcycle = z.infer<typeof motorcycleZodSchema>;
+
+export { motorcycleZodSchema };

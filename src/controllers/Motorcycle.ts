f5b8b16214576cddbadc8b50683e9bd335102a67
Andre Rousselet <andre098@hotmail.com>
2022-10-24 10:55:43 -0300

    feat(controller): add MotorcycleController

---
+++ src/controllers/Motorcycle.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from 'express';
+import { StatusCodes } from '../errors/customError';
+import { IMotorcycle } from '../interfaces/IMotorcycle';
+import { IService } from '../interfaces/IService';
+
+class MotorcycleController {
+  constructor(private _carService: IService<IMotorcycle>) {}
+
+  public async create(req: Request, res: Response<IMotorcycle>) {
+    const createdCar = await this._carService.create(req.body);
+    return res.status(StatusCodes.CREATED).json(createdCar);
+  }
+
+  public async read(_req: Request, res: Response<IMotorcycle[]>) {
+    const carList = await this._carService.read();
+    return res.status(StatusCodes.OK).json(carList);
+  }
+
+  public async readOne(req: Request, res: Response<IMotorcycle | null>) {
+    const foundCar = await this._carService.readOne(req.params.id);
+    return res.status(StatusCodes.OK).json(foundCar);
+  }
+
+  public async update(req: Request, res: Response<IMotorcycle | null>) {
+    const updatedCar = await this._carService.update(req.params.id, req.body);
+    return res.status(StatusCodes.OK).json(updatedCar);
+  }
+
+  public async delete(req: Request, res: Response<IMotorcycle | null>) {
+    await this._carService.delete(req.params.id);
+    return res.status(StatusCodes.NO_CONTENT).end();
+  }
+}
+
+export default MotorcycleController;
